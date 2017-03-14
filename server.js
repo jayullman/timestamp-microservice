fd76d215592559905dff8b4585b3e73114b9f026
Jay Ullman <jayullman@gmail.com>
2017-03-13 22:42:16 -0700

    Satisfy user stories

--- server.js
+++ server.js
@@ -1,8 +1,51 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-  res.send('Home');
+const months = ['January', 'February', 'March', 'April', 'May', 
+                'June', 'July', 'August', 'September', 'October', 
+                'November', 'December'];
+
+app.get('/*', (req, res) => {
+
+  var dateObj;
+  var unixStamp;
+  var natural;
+
+  // extract the string after the home directory
+  const query = decodeURIComponent(req.url.slice(1));
+  
+  if (isNaN(query)) {
+    console.log('string');
+    dateObj = new Date(query);
+
+    // obtain unix time from UTC time
+    unixStamp = dateObj.valueOf() / 1000;
+
+    natural = `${months[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`    
+    
+
+  } else {
+    console.log(('num'));
+    dateObj = new Date(query * 1000);
+    console.log(dateObj);
+    unixStamp = query;
+    natural = `${months[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`    
+    
+  }
+
+  // tests for valid date, if not, returns null for both properties
+  if (natural.indexOf('undefined') > -1 || natural.indexOf('NaN') > -1) {
+    unixStamp = null;
+    natural = null;
+  }
+  
+  var responseObj = {
+    unix: unixStamp,
+    natural: natural
+  }
+
+  res.send(JSON.stringify(responseObj));
+
 });
 
 app.listen(3000, () => {
